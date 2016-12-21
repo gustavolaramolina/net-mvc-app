c1f47f68e7ed29b46c86e08c4e9c0e92ee0dc2b3
Gustavo Lara <gustavo.lara@softtek.com>
2016-12-21 02:06:26 -0600

    Cambio test code pipeline

--- DynamicMenu/Domain/Domain.cs
+++ DynamicMenu/Domain/Domain.cs
@@ -13,7 +13,7 @@ namespace DynamicMenu.Domain
             menu.Add(new Navbar { Id = 1, nameOption = "Menu1", controller = "Home", action = "Index", imageClass = "fa fa-fw fa-dashboard", estatus = true, isParent = false, parentId = 0 });
             menu.Add(new Navbar { Id = 2, nameOption = "Menu2", controller = "Home", action = "Index", imageClass = "fa fa-fw fa-bar-chart-o", estatus = true, isParent = true, parentId = 0 });
             menu.Add(new Navbar { Id = 3, nameOption = "Action", controller = "Home", action = "Dropdown", estatus = true, isParent = false, parentId = 2 });
-            menu.Add(new Navbar { Id = 4, nameOption = "Another action", controller = "Home", action = "Dropdown", estatus = true, isParent = false, parentId = 2 });
+            menu.Add(new Navbar { Id = 4, nameOption = "Another test action", controller = "Home", action = "Dropdown", estatus = true, isParent = false, parentId = 2 });
             menu.Add(new Navbar { Id = 5, nameOption = "Something else here", controller = "Home", action = "Dropdown", estatus = true, isParent = false, parentId = 2 });
             menu.Add(new Navbar { Id = 6, nameOption = "Dropdown", controller = "Home", action = "Dropdown", estatus = true, isParent = true, parentId = 2 });
             menu.Add(new Navbar { Id = 7, nameOption = "Action - 2", controller = "Home", action = "Dropdown", estatus = true, isParent = false, parentId = 6 });
